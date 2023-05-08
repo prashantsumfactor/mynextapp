66425963fc785b82b81409ad4a87630b3b0a8413
Prashant <prashant@sumfactor.com>
2023-05-08 09:27:45 +0530

    add banner in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
-import styles from './page.module.css'
+import styles from './page.module.css';
+import Banner from './component/banner';
 
 const Hello = () =>{
-    return <div className={styles.container}><h1>HEllo World!!</h1></div>
+    return <div className={styles.container}>
+        
+        
+
+        <main>
+
+        <h1>HEllo World!!</h1>
+
+        <Banner/>
+
+        </main>
+
+        </div>
 }
 
 export default Hello;
